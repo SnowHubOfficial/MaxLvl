44b68c603da1f3df19bc19d0d8d33c331e9e1077
SnowHubOfficial <94108542+SnowHubOfficial@users.noreply.github.com>
2021-11-11 20:58:43 +0700

    Update and rename README.md to README.lua

---
+++ README.lua
@@ -0,0 +1,6 @@
+syn.queue_on_teleport([[
+    game:GetService("Players")["LocalPlayer"].Data.Level.Value = 2100
+    game:GetService("Players")["LocalPlayer"].Data.Stats.Melee.Level.Value = 2100
+    game:GetService("Players")["LocalPlayer"].Data.Stats.Defense.Level.Value = 2100
+    game:GetService("Players")["LocalPlayer"].Data.Stats.Gun.Level.Value = 2100
+]])
